c4cda179109347eac5a6a4a33fa04181d26ad5f4
Haï~ <hai-vr@users.noreply.github.com>
2024-12-11 13:56:14 +0100

    Let user mark some objects as local-only or remote-only: - Let the user mark that some objects should only exist local-only, or remote-only. - This lets the user choose to destroy some objects like cameras, displays, local triggers, and such. - Such objects are subject to the same content policing restrictions even if they are local-only, so that the eventual possibility of borrowing avatars would not execute possibly unsafe components to other wearers. - Add Basis.Scripts.BasisSdk.BasisConditionalLoad to the default content police selector.

---
+++ Packages/basisdk/Scripts/BasisConditionalLoad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Basis.Scripts.BasisSdk
+{
+    [DisallowMultipleComponent]
+    public class BasisConditionalLoad : MonoBehaviour
+    {
+        public bool useNetworkCondition = true;
+        public BasisConditionalLoadNetwork networkAllow = BasisConditionalLoadNetwork.Local;
+    }
+
+    [Flags]
+    public enum BasisConditionalLoadNetwork
+    {
+        Local = 1,
+        Remote = 2
+    }
+}
