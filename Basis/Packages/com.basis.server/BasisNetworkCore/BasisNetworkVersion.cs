569fbfce708185fd4909dbc240dc35b9e37e927a
dooly <doolanl208@gmail.com>
2025-08-23 18:26:28 +1000

    readded pooltable

--- Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkVersion.cs
+++ Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkVersion.cs
@@ -2,6 +2,6 @@ namespace Basis.Network.Core
 {
     public class BasisNetworkVersion
     {
-        public static ushort ServerVersion = 12;
+        public static ushort ServerVersion = 13;
     }
 }
