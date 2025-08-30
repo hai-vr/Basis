56ccd594d53036a1747d6f914bb11a65b8c45ff8
dooly <doolanl208@gmail.com>
2025-08-30 21:03:12 +1000

    oki closer

--- Basis/Packages/com.basis.framework/Device Management/Devices/Desktop/BasisAvatarEyeInput.cs
+++ Basis/Packages/com.basis.framework/Device Management/Devices/Desktop/BasisAvatarEyeInput.cs
@@ -157,7 +157,6 @@ namespace Basis.Scripts.Device_Management.Devices.Desktop
 
             // Eye relative to HEAD (short lever)
             Vector3 neutralEyeFromHead = tposeEyeWorld - tposeHeadWorld;
-            Vector3 tposePivotWorld = tposeHeadWorld;
 
             // yaw wrap
             rotationX = Mathf.Repeat(rotationX, 360f);
@@ -173,12 +172,12 @@ namespace Basis.Scripts.Device_Management.Devices.Desktop
                 float heightAdj = (1 - crouchMinimum) * Player.LocalCharacterDriver.CrouchBlend + crouchMinimum;
                 float headLocalY = BasisLocalBoneDriver.HeadControl.TposeLocalScaled.position.y;
                 float crouchDelta = headLocalY * (1 - heightAdj);
-                tposePivotWorld.y -= crouchDelta;
+                tposeHeadWorld.y -= crouchDelta;
             }
 
             // Rotate small head->eye vector
             Vector3 rotatedEyeOffset = targetRot * neutralEyeFromHead;
-            Vector3 eyeWorld = tposePivotWorld + rotatedEyeOffset;
+            Vector3 eyeWorld = tposeHeadWorld + rotatedEyeOffset;
 
             // Output
             UnscaledDeviceCoord.position = eyeWorld;
