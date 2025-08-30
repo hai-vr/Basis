ffed5d55e2e3f8a7ff0b3f1167790bd8a029a714
dooly <doolanl208@gmail.com>
2025-08-30 19:23:44 +1000

    removed headless using

--- Basis/Packages/com.basis.framework/Device Management/Devices/Headless/BasisHeadlessInput.cs
+++ Basis/Packages/com.basis.framework/Device Management/Devices/Headless/BasisHeadlessInput.cs
@@ -1,7 +1,6 @@
 using Basis.Scripts.BasisSdk.Helpers;
 using Basis.Scripts.BasisSdk.Players;
 using Basis.Scripts.Common;
-using Basis.Scripts.Device_Management.Devices.Desktop;
 using Basis.Scripts.Drivers;
 using Basis.Scripts.TransformBinders.BoneControl;
 using System.Collections;
