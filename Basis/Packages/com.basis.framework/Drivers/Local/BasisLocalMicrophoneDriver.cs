6208a7f4497324ab0ce9bc0cb5f28a53e8a61d8f
dooly <doolanl208@gmail.com>
2025-10-09 17:27:18 +1000

    added back

--- Basis/Packages/com.basis.framework/Drivers/Local/BasisLocalMicrophoneDriver.cs
+++ Basis/Packages/com.basis.framework/Drivers/Local/BasisLocalMicrophoneDriver.cs
@@ -110,7 +110,7 @@ public static class BasisLocalMicrophoneDriver
 
     // Temp buffers for denoiser wet/dry and chunking
     private static float[] _denoiseDry; // copy of pre-denoise frame
-
+    private static float[] _tmp480;     // 480-sample scratch (allocated on demand)
     // ---------------------------------------------------------
 
     public static bool Initialize()
@@ -166,6 +166,7 @@ public static class BasisLocalMicrophoneDriver
             processBufferArray = null;
             rmsValues = null;
             _denoiseDry = null;
+            _tmp480 = null;
 
             IsInitialize = false;
             BasisDebug.Log("Microphone Driver Deinitialized.");
