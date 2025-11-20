c0b311965ee3a25af51809cb8dd3cc3869cfea6b
dooly <doolanl208@gmail.com>
2025-11-21 06:57:26 +1000

    added way to unload all items

--- Basis/Packages/com.basis.examples/Scripts/BasisRuntimeLoader.cs
+++ Basis/Packages/com.basis.examples/Scripts/BasisRuntimeLoader.cs
@@ -1,5 +1,7 @@
+using Basis.Scripts.BasisSdk;
 using Basis.Scripts.BasisSdk.Players;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static SerializableBasis;
 
 /// <summary>
@@ -43,6 +45,7 @@ public class BasisRuntimeLoader : MonoBehaviour
 
     private void OnEnable()
     {
+        UnloadAllItemsCheck();
         TryLoad();
     }
 
@@ -104,7 +107,38 @@ public class BasisRuntimeLoader : MonoBehaviour
             BasisNetworkSpawnItem.RequestGameObjectUnLoad(LoadedResource.LoadedNetID);
         }
     }
-
+    public bool UnloadAllLoadedItems = false;
+    public void UnloadAllItemsCheck()
+    {
+        if (UnloadAllLoadedItems)
+        {
+            int count = SceneManager.sceneCount;
+            for (int SceneIndex = 0; SceneIndex < count; SceneIndex++)
+            {
+                Scene Scene = SceneManager.GetSceneAt(SceneIndex);
+                GameObject[] Roots = Scene.GetRootGameObjects();
+                foreach (GameObject Root in Roots)
+                {
+                    BasisContentBase[] bases = Root.GetComponentsInChildren<BasisContentBase>(true);
+                    for (int Index = 0; Index < bases.Length; Index++)
+                    {
+                        BasisContentBase Base = bases[Index];
+                        if (Base.TryGetNetworkGUIDIdentifier(out string id))
+                        {
+                            if (Base is BasisScene)
+                            {
+                                BasisNetworkSpawnItem.RequestSceneUnLoad(id);
+                            }
+                            else
+                            {
+                                BasisNetworkSpawnItem.RequestGameObjectUnLoad(id);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 #if UNITY_EDITOR
     // Keep fields tidy in the editor.
     private void OnValidate()
