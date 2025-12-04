a8cd5d53cbaed33d5fb8e8aaf68425daf10603cd
dooly <doolanl208@gmail.com>
2025-12-04 12:16:47 +1000

    removed the #if check around spans we already are using them elsewhere

--- Basis Server/BasisNetworkCore/NetDataWriter.cs
+++ Basis Server/BasisNetworkCore/NetDataWriter.cs
@@ -35,13 +35,10 @@ namespace Basis.Network.Core {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _position;
         }
-
-#if LITENETLIB_SPANS || NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1 || NETCOREAPP3_1 || NET5_0 || NETSTANDARD2_1
         public ReadOnlySpan<byte> AsReadOnlySpan()
         {
             return new ReadOnlySpan<byte>(_data, 0, _position);
         }
-#endif
 
         public static readonly ThreadLocal<UTF8Encoding> uTF8Encoding = new ThreadLocal<UTF8Encoding>(() => new UTF8Encoding(false, true));
 
