b9ee0c1546146702d8cf82bbca2ac4e0a5b1221b
dooly <doolanl208@gmail.com>
2026-01-03 08:10:50 +1000

    Revert "need to readd documentation for remote player, tomorrows problem"
    This reverts commit aa7240cd9a353a3198731d23efbc163abdbfd289.

--- Basis/Packages/com.basis.framework/UI Panels/BasisIndividualPlayerSettings.cs
+++ Basis/Packages/com.basis.framework/UI Panels/BasisIndividualPlayerSettings.cs
@@ -120,7 +120,7 @@ public class BasisIndividualPlayerSettings : BasisUIBase
 
         if (RemotePlayer != null)
         {
-            RemotePlayer.RequestAvatarStateRefresh();
+            RemotePlayer.ReloadAvatar();
         }
     }
 
@@ -134,7 +134,7 @@ public class BasisIndividualPlayerSettings : BasisUIBase
 
         if (RemotePlayer != null)
         {
-            RemotePlayer.RequestAvatarStateRefresh();
+            RemotePlayer.ReloadAvatar();
         }
     }
 
