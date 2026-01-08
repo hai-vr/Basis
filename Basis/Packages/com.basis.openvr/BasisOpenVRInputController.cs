f568ef29b62718637e0a98632489cfb173a55304
dooly <doolanl208@gmail.com>
2026-01-08 11:48:47 +1000

    trying crims values for controller rotation offsets

--- Basis/Packages/com.basis.openvr/BasisOpenVRInputController.cs
+++ Basis/Packages/com.basis.openvr/BasisOpenVRInputController.cs
@@ -28,8 +28,8 @@ namespace Basis.Scripts.Device_Management.Devices.OpenVR
             HandBiasSplay = -0.8f;
 
             // existing hand rotation offsets
-            leftHandToIKRotationOffset = new Vector3(90, -90, 0);
-            rightHandToIKRotationOffset = new Vector3(90, -90, 0);
+            leftHandToIKRotationOffset = new Vector3(105, 185, 265);
+            rightHandToIKRotationOffset = new Vector3(105, 185,265);
 
             leftHandToIKPositionOffset = new Vector3(-0.02f, 0.07f, 0.02f);
             rightHandToIKPositionOffset = new Vector3(-0.02f, 0.07f, 0.02f);
