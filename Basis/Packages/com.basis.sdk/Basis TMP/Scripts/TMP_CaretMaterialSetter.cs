5200d00c71a31a1ae765dda8cfb3dc0b201f02f7
Auzlex <edwardscharlesuk@googlemail.com>
2026-02-28 17:11:27 +0000

    fixed PE Text Fields missing carrets with new shader changes
    currently TMP does not expose TMP_SelectionCaret, and defaults its ui material to unity's default unity UI material. this quick script will attempt to set the material once on transform change and success of finding TMP_SelectionCaret in the text field boxes. Once set it will adjust the material to the correctly assigned one, so you can see it in the UI again on top of world objects.

---
+++ Basis/Packages/com.basis.sdk/Basis TMP/Scripts/TMP_CaretMaterialSetter.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+namespace Basis.BasisUI
+{    
+    // I absolutely hate everything about this
+    // but TMP does not expose the TMP_SelectionCaret and sets the default material to unity default UI material
+    // and does not derive it from font asset
+    // https://docs.unity3d.com/Packages/com.unity.textmeshpro@3.2/api/TMPro.TMP_SelectionCaret.html
+    [ExecuteAlways] // Enables the callback in both Play and Edit modes
+    public class TMP_CaretMaterialSetter : MonoBehaviour
+    {
+        [SerializeField] private Material desiredMaterial;
+        private bool applied = false;
+
+        void OnTransformChildrenChanged()
+        {
+            if (applied) return;
+
+            // Look for TMP_SelectionCaret among children
+            var caret = GetComponentInChildren<TMP_SelectionCaret>(true);
+            if (caret != null)
+            {
+                //BasisDebug.LogWarning("caret is found setting material");
+                caret.material = desiredMaterial;
+
+                applied = true; // only run once
+            }
+            // else
+            // {
+            //     BasisDebug.LogError("caret is null");
+            // }
+        }
+    }
+}
