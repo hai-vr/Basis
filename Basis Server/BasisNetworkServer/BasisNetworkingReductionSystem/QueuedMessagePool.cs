fe128cde8fcd69f8fc8bac54e4c1018c389e5d7e
luke doolan <doolanl208@gmail.com>
2026-03-06 01:05:19 +1000

    recycled more data & improved queue / pool logic

--- Basis Server/BasisNetworkServer/BasisNetworkingReductionSystem/QueuedMessagePool.cs
+++ Basis Server/BasisNetworkServer/BasisNetworkingReductionSystem/QueuedMessagePool.cs
@@ -3,17 +3,18 @@ namespace BasisNetworkServer.BasisNetworkingReductionSystem
 {
     public class QueuedMessagePool
     {
-        private static ConcurrentBag<QueuedMessage> pool = new();
+        private static readonly ConcurrentQueue<QueuedMessage> pool = new();
 
         public static QueuedMessage Rent()
         {
-            return pool.TryTake(out var msg) ? msg : new QueuedMessage();
+            return pool.TryDequeue(out var msg) ? msg : new QueuedMessage();
         }
 
         public static void Return(QueuedMessage msg)
         {
             msg.FromPeer = null;
-            pool.Add(msg);
+            msg.AvatarMessage = default;
+            pool.Enqueue(msg);
         }
     }
 }
