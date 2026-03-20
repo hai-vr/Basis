e7b2575fcd3b4a68131c43f8b8b22b128bd8efd3
luke doolan <doolanl208@gmail.com>
2026-03-21 09:01:31 +1000

    ready to roll

--- Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkVersion.cs
+++ Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkVersion.cs
@@ -2,6 +2,6 @@ namespace Basis.Network.Core
 {
     public class BasisNetworkVersion
     {
-        public static ushort ServerVersion = 24;
+        public static ushort ServerVersion = 25;
     }
 }
