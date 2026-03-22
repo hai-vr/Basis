7eb8b2a546d7097623aceaa2d5c4580beac2c897
luke doolan <doolanl208@gmail.com>
2026-03-22 22:15:56 +1000

    removed handle check

--- Basis Server/BasisNetworkServer/BasisNetworkMessageProcessor/BasisNetworkMessageProcessor.cs
+++ Basis Server/BasisNetworkServer/BasisNetworkMessageProcessor/BasisNetworkMessageProcessor.cs
@@ -220,27 +220,6 @@ public static class BasisNetworkMessageProcessor
             reader.Recycle();
         }
     }
-
-    private static void HandleAdminResourceAction(NetPeer peer, NetPacketReader reader, Action<NetPacketReader, NetPeer,string> action, string permNode)
-    {
-        if (!NetworkServer.AuthIdentity.NetIDToUUID(peer, out string uuid))
-        {
-            BNL.LogError($"User UUID not found for peer: {peer}");
-            reader.Recycle();
-            return;
-        }
-
-        if (PermissionIntegration.HasRequirement(uuid, permNode) ||
-            PermissionIntegration.HasRequirement(uuid, PermNodes.All))
-        {
-            action(reader, peer, uuid); // recycles inside handler
-        }
-        else
-        {
-            BNL.LogError($"Unauthorized access attempt by UUID: {uuid} for {permNode}");
-            reader.Recycle();
-        }
-    }
     private static bool TryWithPermission(NetPeer peer, NetPacketReader reader, string permNode, out string uuid)
     {
         if (!NetworkServer.AuthIdentity.NetIDToUUID(peer, out uuid))
