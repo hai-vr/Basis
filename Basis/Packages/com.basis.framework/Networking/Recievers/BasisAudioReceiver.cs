881594dc96aab9129f2b378420e5cec791ff6014
TimothySoup <contact@timothysoup.dev>
2026-03-23 23:15:05 +0100

    Fixed the master audio not affecting player voices (#673)
    * fixed the master audio not affecting player voices
    
    * update listener volume to use SMModuleAudio.ActiveMainVolume

--- Basis/Packages/com.basis.framework/Networking/Recievers/BasisAudioReceiver.cs
+++ Basis/Packages/com.basis.framework/Networking/Recievers/BasisAudioReceiver.cs
@@ -589,10 +589,11 @@ namespace Basis.Scripts.Networking.Receivers
             Buffer.BlockCopy(segment, 0, _inputScratch, 0, frames * sizeof(float));
 
             float dampen = DirectionalDampeningMultiplier;
+            float listenerVolume = SMModuleAudio.ActiveMainVolume;
             int idx = 0;
             for (int f = 0; f < frames; f++)
             {
-                float sample = FastClamp(_inputScratch[f]) * dampen;
+                float sample = FastClamp(_inputScratch[f] * dampen * listenerVolume);
                 for (int c = 0; c < channels; c++)
                 {
                     data[idx++] = sample;
@@ -647,10 +648,11 @@ namespace Basis.Scripts.Networking.Receivers
             }
 
             float dampen = DirectionalDampeningMultiplier;
+            float listenerVolume = SMModuleAudio.ActiveMainVolume;
             int idx = 0;
             for (int f = 0; f < frames; f++)
             {
-                float sample = FastClamp(_resampleScratch[f]) * dampen;
+                float sample = FastClamp(_resampleScratch[f] * dampen * listenerVolume);
                 for (int c = 0; c < channels; c++)
                 {
                     data[idx++] = sample;
