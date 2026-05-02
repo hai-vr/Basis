1316afc837f19e01d1233e1c1d277f0e1ebd2e3e
luke doolan <doolanl208@gmail.com>
2026-05-02 15:47:32 +1000

    getting ready to merge, increase server version now that its been a week

--- Basis Server/BasisNetworkCore/BasisNetworkVersion.cs
+++ Basis Server/BasisNetworkCore/BasisNetworkVersion.cs
@@ -2,6 +2,6 @@ namespace Basis.Network.Core
 {
     public class BasisNetworkVersion
     {
-        public static ushort ServerVersion = 31;
+        public static ushort ServerVersion = 32;
     }
 }
