e629a6313f7e607b7efb387ff856b91e635fd75d
Toys0125 <marcus.g.stange@gmail.com>
2026-06-07 00:25:40 -0500

    add unity server flag

--- Basis/Packages/com.basis.framework/BasisUI/Menus/Main Menu Providers/SettingsProviderParts/SettingsProviderNamePlate.cs
+++ Basis/Packages/com.basis.framework/BasisUI/Menus/Main Menu Providers/SettingsProviderParts/SettingsProviderNamePlate.cs
@@ -97,7 +97,9 @@ namespace Basis.BasisUI
         public static void ApplyNamePlateSettings()
         {
             BasisRemoteNamePlateDriver.ApplyNamePlateSettingsFromUI();
+#if !UNITY_SERVER
             BasisNetworkPIPCameraDriver.ApplyPipNamePlateSettingsFromUI();
+#endif        
         }
     }
 }
