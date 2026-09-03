1adb5b85441e3a67051e62a6845c5a88e750ebcb
Haï~ <hai-vr@users.noreply.github.com>
2026-09-03 07:37:36 +0200

    Print limited username.

--- Basis Server/BasisNetworkServer/Networking/HVREventLog.cs
+++ Basis Server/BasisNetworkServer/Networking/HVREventLog.cs
@@ -1,9 +1,22 @@
 ﻿using Basis.Network.Core;
+using BasisPermissions;
 
 namespace BasisNetworkServer.Networking;
 
 public static class HVREventLog
 {
+    public static string LimitedUsername(NetPeer peer)
+    {
+        if (NetworkServer.AuthIdentity != null &&
+            NetworkServer.AuthIdentity.NetIDToUUID(peer, out string uuid) &&
+            PermissionManager.PermissionIntegration.TryGetPlayerMeta(uuid, out var meta))
+        {
+            return BasisDisplayNameSanitizer.LimitUsername(meta.playerDisplayName);
+        }
+
+        return "???";
+    }
+    
     public static void PreLog(string message, ushort senderId)
     {
         if (NetworkServer.AuthenticatedPeers.TryGetValue(senderId, out var peer))
@@ -12,18 +25,18 @@ public static class HVREventLog
         }
         else
         {
-            PreLog(message, senderId, "???", "???");
+            PreLog(message, senderId, "???", "???", "???");
         }
     }
     
     public static void PreLog(string message, NetPeer peer)
     {
-        PreLog(message, peer.Id, DidOrDefault(peer), peer.Address.ToString());
+        PreLog(message, peer.Id, DidOrDefault(peer), peer.Address.ToString(), LimitedUsername(peer));
     }
 
-    private static void PreLog(string message, int id, string did, string address)
+    private static void PreLog(string message, int id, string did, string address, string username)
     {
-        BNL.Log($"[EVENT] {message} #{id} ({did}) [{address}]");
+        BNL.Log($"[EVENT] {message} #{id} {username} ({did}) [{address}]");
     }
 
     private static string DidOrDefault(NetPeer peer)
