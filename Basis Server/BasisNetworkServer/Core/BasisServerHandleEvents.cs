f0dadf47a9cff140dc140fd6886717810b13fb3d
Haï~ <hai-vr@users.noreply.github.com>
2026-09-05 05:33:59 +0200

    Misc update logging.

--- Basis Server/BasisNetworkServer/Core/BasisServerHandleEvents.cs
+++ Basis Server/BasisNetworkServer/Core/BasisServerHandleEvents.cs
@@ -468,7 +468,6 @@ namespace BasisServerHandle
                 }
                 int id = peer.Id;
 
-                var hasLastPlayerMetaData = BasisSavedState.GetLastPlayerMetaData(peer, out ClientMetaDataMessage meta);
                 lock (_joinLock)
                 {
                     bool slotHeldByAnother = NetworkServer.AuthenticatedPeers.TryGetValue(id, out NetPeer holder) && !Equals(holder, peer);
@@ -477,14 +476,7 @@ namespace BasisServerHandle
                     {
                         NetworkServer.RebuildPeerSnapshot();
                         BNL.Log($"Peer removed: {id}");
-                    if (hasLastPlayerMetaData)
-                    {
-                        HVREventLog.PreLog($"User left: {meta.playerDisplayName}", peer);
-                    }
-                    else
-                    {
-                        HVREventLog.PreLog("User left: unknown", peer);
-                    }
+                        HVREventLog.PreLog("User left", peer);
                     }
                     else if (slotHeldByAnother)
                     {
@@ -1592,7 +1584,7 @@ namespace BasisServerHandle
             switch (LocalLoadResource.LoadStrategy)
             {
                 case 0:
-                    BasisNetworkResourceManagement.LoadResource(LocalLoadResource);
+                    BasisNetworkResourceManagement.LoadResource(LocalLoadResource, Peer);
                     break;
                 case 2: // Synchronized
                     BasisNetworkPreloadResourceManagement.StartSynchronizedLoad(LocalLoadResource);
@@ -1602,7 +1594,7 @@ namespace BasisServerHandle
                     break;
                 default:
                     BNL.LogError("Falling Back to Resource Load, Unsupported Load Strategy");
-                    BasisNetworkResourceManagement.LoadResource(LocalLoadResource);
+                    BasisNetworkResourceManagement.LoadResource(LocalLoadResource, Peer);
                     break;
             }
         }
