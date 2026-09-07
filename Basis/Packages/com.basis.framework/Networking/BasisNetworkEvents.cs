5350a06bca60e855f137c7bbb17c8b8ab2704cd1
Haï~ <hai-vr@users.noreply.github.com>
2026-09-07 13:25:21 +0200

    feat(PubSub): server can send messages to props

--- Basis/Packages/com.basis.framework/Networking/BasisNetworkEvents.cs
+++ Basis/Packages/com.basis.framework/Networking/BasisNetworkEvents.cs
@@ -632,6 +632,16 @@ public static class BasisNetworkEvents
             BasisP2PManager.HandleServerMessage(Reader);
         });
 
+        BasisClientMessageRegistry.RegisterCore(BasisNetworkCommons.PubSubChannel, (peer, Reader, channel, deliveryMethod) =>
+        {
+            if (ValidateSize(Reader, peer, channel) == false)
+            {
+                Reader.Recycle();
+                return;
+            }
+            BasisNetworkHandlePubSub.HandleMessage(Reader, deliveryMethod);
+        });
+
         BasisClientMessageRegistry.RegisterCore(BasisNetworkCommons.EventsChannel, (peer, Reader, channel, deliveryMethod) =>
         {
             if (ValidateSize(Reader, peer, channel) == false)
