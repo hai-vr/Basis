7a276a23d1a12426e7c8e2f5f2f496edc028731a
Haï~ <hai-vr@users.noreply.github.com>
2026-09-07 10:36:20 +0200

    feat(PubSub): implement shim for PubSub (*)

---
+++ Basis/Packages/com.basis.framework/Networking/Handles/BasisNetworkHandlePubSub.cs
@@ -0,0 +1,23 @@
+﻿using Basis.Network.Core;
+
+namespace Basis.Scripts.Networking
+{
+    public static class BasisNetworkHandlePubSub
+    {
+        public delegate void PubSubMessageDelegate(byte[] buffer, DeliveryMethod deliveryMethod);
+        public static event PubSubMessageDelegate OnPubSubMessageReceived;
+
+        public static void HandleMessage(NetPacketReader reader, DeliveryMethod deliveryMethod)
+        {
+            try
+            {
+                byte[] data = reader.GetRemainingBytes();
+                OnPubSubMessageReceived?.Invoke(data, deliveryMethod);
+            }
+            finally
+            {
+                reader.Recycle();
+            }
+        }
+    }
+}
