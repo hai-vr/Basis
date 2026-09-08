b596c1528c22283f1a738c4e98f3baa2513e6ee2
Haï~ <hai-vr@users.noreply.github.com>
2026-09-08 05:09:35 +0200

    feat(PubSub): WIP add channel name matching, still need to modify the shim so that when the channel name is received, we check if our pattern accepts it

--- Basis Server/BasisNetworkCore/Serializable/Protocol/BasisCustomServerDataMessages.cs
+++ Basis Server/BasisNetworkCore/Serializable/Protocol/BasisCustomServerDataMessages.cs
@@ -6,18 +6,18 @@ public static partial class SerializableBasis
     [Serializable]
     public struct CustomServerDataSubscribeRequest
     {
-        public string ChannelName;
+        public string ChannelPattern;
         public Guid RequestID;
 
         public void Serialize(NetDataWriter writer)
         {
-            writer.Put(ChannelName);
+            writer.Put(ChannelPattern);
             writer.Put(RequestID);
         }
 
         public bool Deserialize(NetDataReader reader)
         {
-            if (reader.TryGetString(out ChannelName) && reader.AvailableBytes >= 16)
+            if (reader.TryGetString(out ChannelPattern) && reader.AvailableBytes >= 16)
             {
                 RequestID = reader.GetGuid();
                 return true;
@@ -30,18 +30,18 @@ public static partial class SerializableBasis
     [Serializable]
     public struct CustomServerDataUnsubscribeRequest
     {
-        public string ChannelName;
+        public string ChannelPattern;
         public Guid RequestID;
 
         public void Serialize(NetDataWriter writer)
         {
-            writer.Put(ChannelName);
+            writer.Put(ChannelPattern);
             writer.Put(RequestID);
         }
 
         public bool Deserialize(NetDataReader reader)
         {
-            if (reader.TryGetString(out ChannelName) && reader.AvailableBytes >= 16)
+            if (reader.TryGetString(out ChannelPattern) && reader.AvailableBytes >= 16)
             {
                 RequestID = reader.GetGuid();
                 return true;
