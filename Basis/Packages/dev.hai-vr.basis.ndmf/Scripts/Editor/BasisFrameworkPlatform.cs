2dc8bb1cea95692a3a4323304ff1f27437ea454b
Haï~ <hai-vr@users.noreply.github.com>
2026-09-13 02:49:22 +0200

    feat(ndmf): add NDMF JiggleRig conversion

--- Basis/Packages/dev.hai-vr.basis.ndmf/Scripts/Editor/BasisFrameworkPlatform.cs
+++ Basis/Packages/dev.hai-vr.basis.ndmf/Scripts/Editor/BasisFrameworkPlatform.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Basis.Scripts.BasisSdk;
 using nadena.dev.ndmf.platform;
 using UnityEditor;
 using UnityEngine;
+#if BASISNDMF_JIGGLERIG_IS_INSTALLED
+using GatorDragonGames.JigglePhysics;
+#endif
+using nadena.dev.ndmf.multiplatform.components;
+using Unity.Mathematics;
 
 namespace HVR.Basis.NDMF
 {
@@ -108,6 +114,125 @@ namespace HVR.Basis.NDMF
             name = mesh.GetBlendShapeName(blendShapeIndex);
             return true;
         }
+
+#if BASISNDMF_JIGGLERIG_IS_INSTALLED
+        public void GeneratePortableComponents(GameObject root, bool useUndo)
+        {
+            Dictionary<Transform, PortableDynamicBone> explicitDynBones = new();
+            Dictionary<ColliderEqualityCheck, PortableDynamicBoneCollider> equalityCheckToPortableCollider = new();
+
+            foreach (var pdb in root.GetComponentsInChildren<PortableDynamicBone>(true))
+            {
+                if (pdb.Root != null) explicitDynBones[pdb.Root] = pdb;
+            }
+            
+            var allJiggleRig = root.GetComponentsInChildren<JiggleRig>(true);
+            foreach (var jiggleRig in allJiggleRig)
+            {
+                foreach (JiggleColliderSerializable colliderSer in jiggleRig.GetJiggleRigData().jiggleColliders)
+                {
+                    var collider = colliderSer.collider;
+
+                    var key = ToEqualityCheck(collider);
+                    if (!equalityCheckToPortableCollider.ContainsKey(key))
+                    {
+                        var portable = jiggleRig.gameObject.AddComponent<PortableDynamicBoneCollider>();
+                        equalityCheckToPortableCollider[key] = portable;
+                        
+                        portable.ColliderType = collider.type switch
+                        {
+                            JiggleCollider.JiggleColliderType.Sphere => PortableDynamicColliderType.Sphere,
+                            JiggleCollider.JiggleColliderType.Capsule => PortableDynamicColliderType.Capsule,
+                            _ => PortableDynamicColliderType.Sphere
+                        };
+                        portable.Radius = collider.worldRadius;
+                        portable.Height = collider.worldHeight;
+                        portable.PositionOffset = collider.localOffset;
+                        portable.RotationOffset = Quaternion.identity;
+                        portable.InsideBounds = false;
+                    }
+                }
+            }
+
+            foreach (var jr in allJiggleRig)
+            {
+                var jrData = jr.GetJiggleRigData();
+                var jrCollisionRadius = jrData.jiggleTreeInputParameters.collisionRadius;
+                
+                var rootBone = jrData.rootBone ?? jr.transform;
+                var portable = explicitDynBones.GetValueOrDefault(rootBone) ??
+                               jr.gameObject.AddComponent<PortableDynamicBone>();
+
+                portable.enabled = jr.enabled;
+                portable.BaseRadius.WeakSet(jrCollisionRadius.value);
+                portable.IsGrabbable.WeakSet(!jrData.lockFromGrabbing);
+                portable.IgnoreSelf.WeakSet(false);
+                portable.IgnoreTransforms.WeakSet(jrData.excludedTransforms.Where(transform => transform != null).ToList());
+                
+                portable.Root = rootBone;
+                portable.Colliders.WeakSet(jrData.jiggleColliders.Select(c => equalityCheckToPortableCollider[ToEqualityCheck(c.collider)]).ToList());
+                portable.IgnoreMultiChild.WeakSet(jrData.excludeRoot); // TODO: Is this the correct thing?
+
+                if (jrCollisionRadius.curveEnabled)
+                {
+                    portable.RadiusCurve.WeakSet(jrCollisionRadius.curve);
+                }
+            }
+        }
+        
+        private struct ColliderEqualityCheck : IEquatable<ColliderEqualityCheck>
+        {
+            public bool enabled;
+            public JiggleCollider.JiggleColliderType type;
+            public float radius;
+            public float worldRadius;
+            public float height;
+            public float worldHeight;
+            public JiggleCollider.CapsuleAxis capsuleAxis;
+            public float3 localOffset;
+
+            public bool Equals(ColliderEqualityCheck other)
+            {
+                return enabled == other.enabled && type == other.type && radius.Equals(other.radius) && worldRadius.Equals(other.worldRadius) && height.Equals(other.height) && worldHeight.Equals(other.worldHeight) && capsuleAxis == other.capsuleAxis && localOffset.Equals(other.localOffset);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is ColliderEqualityCheck other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = enabled.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (int)type;
+                    hashCode = (hashCode * 397) ^ radius.GetHashCode();
+                    hashCode = (hashCode * 397) ^ worldRadius.GetHashCode();
+                    hashCode = (hashCode * 397) ^ height.GetHashCode();
+                    hashCode = (hashCode * 397) ^ worldHeight.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (int)capsuleAxis;
+                    hashCode = (hashCode * 397) ^ localOffset.GetHashCode();
+                    return hashCode;
+                }
+            }
+        }
+
+        private static ColliderEqualityCheck ToEqualityCheck(JiggleCollider collider)
+        {
+            return new ColliderEqualityCheck
+            {
+                enabled = collider.enabled,
+                type = collider.type,
+                radius = collider.radius,
+                worldRadius = collider.worldRadius,
+                height = collider.height,
+                worldHeight = collider.worldHeight,
+                capsuleAxis = collider.capsuleAxis,
+                localOffset = collider.localOffset
+            };
+        }
+#endif
     }
 
     public class BasisFrameworkBuildUI : BuildUIElement
